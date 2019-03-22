24cb947b13305a623c4b5a3ff99bf649d510e173
Caleb Masters <caleb.masters@endurance.com>
2019-03-21 17:01:54 -0700

    Creating list of moves based on programatic filtering

---
+++ src/components/WorkoutList.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState, useContext } from 'react'
+import Move from '../components/Move'
+
+function WorkoutList() {
+  const { alternateDay, setAlternateDay } = useState( false )
+  const [ displayList, setDisplayList ] = useState([
+    {
+      id: 1,
+      name: "squats"
+    },
+    {
+      id: 2,
+      name: "squats"
+    }
+  ])
+
+  return (
+    <>
+      <div>
+        Toggle { alternateDay ? 'second' : 'first'}
+      </div>
+      <div>
+        Date information
+      </div>
+      <div className="list-area">
+        {displayList.map(move =>
+          <Move
+            key={move.id}
+            id={move.id}
+            name={move.name}
+            />  
+        )}
+      </div>
+    </>
+  )
+}
+
+export default WorkoutList 
